77e39515e8118492d8e97055575f5b1ca57ae399
Saheed Adepoju <adepoju@gmail.com>
2023-09-25 23:56:30 -0700

    Update for guessing code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
@@ -7,11 +8,24 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1,101);
     println!("The secret number is {}", secret_number);
-    let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
+    
+     let mut guess = String::new();
+     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+     let guess: u32 = guess.trim().parse()
+    .expect("Please enter a number");
+
+
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Equal => println!("You win"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Less => println!("Too small")
+    }
+
 
 }
