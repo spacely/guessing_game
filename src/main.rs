07ac7c2f470ebe147921227329fbb2ee042f254b
Saheed Adepoju <adepoju@gmail.com>
2023-09-26 22:49:23 -0700

    Final conclusion of guessing game

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,36 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
-    println!("Please input your number guess");
+    //println!("Please input your number guess");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("The secret number is {}", secret_number);
+    //println!("The secret number is {}", secret_number);
 
-    
+    loop{
+    println!("Please input your number guess");
      let mut guess = String::new();
      io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-     let guess: u32 = guess.trim().parse()
-    .expect("Please enter a number");
+     let guess: u32 =  match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) =>continue,
+    };
+    
 
 
 
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number){
-        Ordering::Equal => println!("You win"),
+        Ordering::Equal => {
+            println!("You win");
+            break;
+        },
         Ordering::Greater => println!("Too big"),
         Ordering::Less => println!("Too small")
     }
+}
 
 
 }
